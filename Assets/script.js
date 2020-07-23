ccefeb75b6c0f0e05437b7e3ab4df438685e359a
Devin Castro <dcrocker21@gmail.com>
2020-07-23 10:21:21 -0700

    used arrow function syntax

--- Assets/script.js
+++ Assets/script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 
 
 // First lets create a function and call it generatePassword.
-function generatePassword() {
+const generatePassword = _ => {
 
   let upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   let lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
